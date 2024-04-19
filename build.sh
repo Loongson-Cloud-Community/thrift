f5d4b57c02b53db2689235bcf3981e35ff1c942e
zhaixiaojuan <zhaixiaojuan@loongson.cn>
2024-04-18 22:57:03 -0400

    Add loongarch64 support

---
+++ build.sh
@@ -0,0 +1,18 @@
+## ¿¿¿¿rustup¿¿rust¿
+curl --proto '=https' --tlsv1.2 -sSf https://rust-lang.loongnix.cn/rustup-init.sh
+[root@kubernetes-master-1 thrift]# cat ~/.cargo/config
+
+[source.crates-io]
+replace-with = 'loongnix'
+
+[source.loongnix]
+registry = "https://crates.loongnix.cn/crates.io-index"
+
+rm lib/rs/Cargo.lock
+
+## ¿¿
+## boost¿¿¿/usr/include/boost ¿¿¿
+yum install -y boost-devel  libtool gcc gcc-c++ make openssl-libs openssl-devel rustfmt clippy rust-toolset flex
+./bootstrap.sh  ##¿¿configure
+./configure  --with-swift=no  --prefix=/usr/local/zxj  ## --prefix ¿¿¿¿¿¿¿--enable-tests=no ¿¿¿¿
+make
